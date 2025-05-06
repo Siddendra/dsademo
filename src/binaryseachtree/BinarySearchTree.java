50c96d203b4d0ac5680eb451a3bb0ddfa19615af
Siddendra Kodepalli <siddendra@gmail.com>
2025-05-06 10:43:35 +0530

    BinarySearch Tree

---
+++ src/binaryseachtree/BinarySearchTree.java
@@ -0,0 +1,19 @@
+package binaryseachtree;
+
+public class BinarySearchTree {
+    private Node root;
+
+    class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+}
