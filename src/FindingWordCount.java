f5650ccfcbc83e97883b4c2c3f33e1f65f1ff710
Siddendra Kodepalli <siddendra@gmail.com>
2025-06-12 20:34:35 +0530

    Find word Count

--- src/FindingWordCount.java
+++ src/FindingWordCount.java
@@ -1,2 +1,60 @@
-package PACKAGE_NAME;public class FindingWordCount {
+import java.io.*;
+import java.util.*;
+
+/**
+ * Input: Path of text file, list of words to search.
+ * Output: The number of instances of each word in the list in the text file.
+ *
+ * Given a path to a text file and a list of words, return the count of each word in the list in the text file.
+ *
+ *
+ * func countOccurences(filePath string, []string wordsToSearch) map[string] int {
+ * }
+ */
+
+public class FindingWordCount {
+
+    public static void main(String[] args) {
+
+
+        String filepath = "./path.txt";
+        List<String> wordsToSearch = Arrays.asList("pen", "pencile", "light");
+        //Set<String> wordsSet = new HashSet<>();
+        Map<String, Integer> wordsToCount = new HashMap<>();
+
+        for(String word: wordsToSearch) {
+            wordsToCount.put(word.toLowerCase(), 0);
+        }
+
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(filepath));
+            String line;
+            while((line = bufferedReader.readLine()) != null) {
+
+                String[] words = line.split("[^a-zA-Z]+");
+
+                for(String word: words) {
+                    String lowerWord= word.toLowerCase();
+                    if(wordsToSearch.contains(lowerWord)) {
+                        int count = wordsToCount.get(lowerWord);
+                        wordsToCount.put(lowerWord,  count++);
+                    }
+
+                }
+
+
+
+            }
+
+
+        }catch (FileNotFoundException ex) {
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
 }
