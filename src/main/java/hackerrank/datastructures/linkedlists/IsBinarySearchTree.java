8eb778e8a99db8a13b85edd9787480da7f600eea
Siddendra Kodepalli <siddendra@gmail.com>
2026-02-10 11:28:24 +0530

    IsBinarysearchTree

--- src/main/java/hackerrank/datastructures/linkedlists/IsBinarySearchTree.java
+++ src/main/java/hackerrank/datastructures/linkedlists/IsBinarySearchTree.java
@@ -1,2 +1,32 @@
-package main.java.hackerrank.datastructures.linkedlists;public class IsBinarySearchTree {
+package main.java.hackerrank.datastructures.linkedlists;
+
+import main.java.util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class IsBinarySearchTree {
+
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) return true;
+
+        List<Integer> inorderList = new ArrayList<>();
+        helper(root, inorderList);
+
+        int prev = inorderList.get(0);
+        for (int i = 1; i < inorderList.size(); i++) {
+            int cur = inorderList.get(i);
+            if (cur <= prev) return false;
+            prev = cur;
+        }
+        return true;
+    }
+
+    void helper(TreeNode node, List<Integer> inorderList) {
+        if (node == null) return;
+        helper(node.left, inorderList);
+        inorderList.add(node.val);
+        helper(node.right, inorderList);
+    }
 }
